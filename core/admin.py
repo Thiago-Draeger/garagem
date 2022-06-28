c8338a90d9bbee3f7a680a1070d620fc89711919
Thiago Draeger <thiagodraeger123@gmail.com>
2022-06-28 14:55:16 -0300

    commit inicial

---
+++ core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from django.contrib import admin
+
+from core.models import Categoria
+from core.models import Marca
+
+admin.site.register(Categoria)
+admin.site.register(Marca)
